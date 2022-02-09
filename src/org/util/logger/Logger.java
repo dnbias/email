c7975d651d6fa67b9ecb10ba707f59a3fd60832a
dnbias <daniel.biasiotto@edu.unito.it>
2022-02-10 00:36:43 +0100

    Fixes and better formatting

--- src/org/util/logger/Logger.java
+++ src/org/util/logger/Logger.java
@@ -1,32 +1,60 @@
 package org.util.logger;
 
+import javafx.application.Platform;
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.Socket;
 import java.util.Date;
 import java.sql.Timestamp;
+import java.util.List;
 
 public class Logger {
-    static String path = "./server.log", messageFinal;
+    static String path = "./server.log";
     static BufferedWriter writer = null;
     static Date date = new Date();
+    static List<String> outList = null;
+
+    public static void log(Socket s, String message) {
+        Timestamp timestamp = new Timestamp(date.getTime());
+        String socket = s.getRemoteSocketAddress().toString().replaceAll("/","");
+        String messageFinal = timestamp + " | " + socket + " - " + message;
+
+        print(messageFinal);
+    }
 
     public static void log(String message) {
         Timestamp timestamp = new Timestamp(date.getTime());
-        messageFinal = timestamp + " | " + message;
-        System.out.println(messageFinal);
-        try {
+        String messageFinal = timestamp + " | " + message;
+
+        print(messageFinal);
+    }
+
+    private static void print(String message) {
+        System.out.println(message); // write to stdout
+
+        try { // write to file
             if (writer == null) {
                 writer = new BufferedWriter(new FileWriter(path,true));
             }
-            writer.append(messageFinal).append("\n");
+            writer.append(message).append("\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        close();
+
+        if (outList != null) { // write to application
+            Platform.runLater(() -> outList.add(message));
+        }
+
+        closeWriter();
     }
 
-    public static void close() {
+    public static void setOutputList(List<String> list) {
+        outList = list;
+    }
+
+    public static void closeWriter() {
         try {
             if (writer != null) {
                 writer.flush();
